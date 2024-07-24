689501e080730fdb1ab724459482f345ceb11e99
Roman Buchser <roman.buchser@gmail.com>
2024-07-24 13:12:11 +0200

    Permit Cross-Origin Resource Sharing in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,33 @@
+using SignalRServiceHub.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLogging(); 
+
+// To allow Progrssive Blazor Apps to access the service
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+builder.Services.AddControllers();
+
+builder.Services.AddSignalR();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapHub<ServiceHub>("/servicehub");
+
+// To allow Progrssive Blazor Apps to access the service
+app.UseCors("AllowAllOrigins");
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints?.MapControllers();
+});
 
 app.Run();
